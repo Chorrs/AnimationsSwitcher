8bc1e3812d1772825b5e666a00457a8d6f55556c
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2024-01-20 22:01:43 +0200

    Initial Commit

---
+++ AnimationsSwitcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsSwitcher
+//
+//  Created by Chorrs on 20.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
